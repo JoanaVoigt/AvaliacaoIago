b2dac51b0066d7a68d296d585c6607cca343757b
JoanaVoigt <joana_voigt@estudante.sesisenai.org.br>
2025-03-04 18:24:18 -0300

    - estruturado

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,24 +1,24 @@
-const connection = require('../Estruturado/database/connection')
+const connection = require('../database/connection')
 const express = require('express')
 const router = express.Router()
-const AWSController = require('../Estruturado/Controller/AWSController')
-const ImageControllerNova = require('../Estruturado/Controller/ImagemController')
-const UserControllerNova = require('../Estruturado/Controller/UsuarioController')
+const AWSController = require('../Controller/AWSController')
+const ImageControllerNova = require('../Controller/ImagemController')
+const UserControllerNova = require('../Controller/UsuarioController')
 
 const multer = require('multer');
 const upload = multer();
 
-router.post('/estruturado/novaImagem', ImageControllerNova.novaImagem)
-router.get('/estruturado/imagens', ImageControllerNova.listarImagem)
-router.get('/estruturado/imagem/:id', ImageControllerNova.buscarImagem)
-router.put('/estruturado/editarImagem/:id', ImageControllerNova.atualizarImagem)
-router.delete('/estruturado/apagarImagem/:id', ImageControllerNova.removerImagem);
+router.post('/novaImagem', ImageControllerNova.novaImagem)
+router.get('/imagens', ImageControllerNova.listarImagem)
+router.get('/imagem/:id', ImageControllerNova.buscarImagem)
+router.put('/editarImagem/:id', ImageControllerNova.atualizarImagem)
+router.delete('/apagarImagem/:id', ImageControllerNova.removerImagem);
 
-router.post('/estruturado/novoUsuario', UserControllerNova.novaUsuario)
-router.get('/estruturado/usuarios', UserControllerNova.listarUsuario)
-router.get('/estruturado/usuario/:id', UserControllerNova.buscarUsuario)
-router.put('/estruturado/editarUsuario/:id', UserControllerNova.atualizarUsuario)
-router.delete('/estruturado/apagarUsuario/:id', UserControllerNova.removerUsuario);
+router.post('/novoUsuario', UserControllerNova.novaUsuario)
+router.get('/usuarios', UserControllerNova.listarUsuario)
+router.get('/usuario/:id', UserControllerNova.buscarUsuario)
+router.put('/editarUsuario/:id', UserControllerNova.atualizarUsuario)
+router.delete('/apagarUsuario/:id', UserControllerNova.removerUsuario);
 
 router.get('/aws', AWSController.buscarImagem)
 router.post('/upload', upload.single('file'), AWSController.uploadFile);
